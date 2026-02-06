da42520c674f891107559b4467d768ed0fea516d
its-daaghi <1muhammadahmad0313@gmail.com>
2026-02-06 22:17:20 +0500

    Add Password Change Features

--- Authentication/customer/views.py
+++ Authentication/customer/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
-
-
+@login_required
 def customer_dashboard_view(request):
     return render(request, "customer/dashboard.html")
 
 
+@login_required
 def password_change_view(request):
-    return render(request, "customer/password_change.html")
+    if request.method == "POST":
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            logout(request)
+            messages.success(
+                request,
+                "Password changed successfully. Please log in with your new password.",
+            )
+            return redirect("login")
+        else:
+            # Handle form errors and display them to the user
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, error)
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, "customer/password_change.html", {"form": form})
